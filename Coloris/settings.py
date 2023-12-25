3a8c2ca56aa32f9c053025fc03c43f85388e364c
lbraun6 <lbraun6@gatech.edu>
2023-12-25 17:04:51 -0500

    Database change

--- Coloris/settings.py
+++ Coloris/settings.py
@@ -64,11 +64,11 @@ SESSION_SAVE_EVERY_REQUEST = True
 
 ROOT_URLCONF = 'Coloris.urls'
 
+
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates']
-        ,
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
