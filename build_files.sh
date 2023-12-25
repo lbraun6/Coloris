17706906472297e4d67138aa8e6d5f2fcd831624
lbraun6 <lbraun6@gatech.edu>
2023-12-25 01:03:41 -0500

    Database change

--- build_files.sh
+++ build_files.sh
@@ -1,6 +1,6 @@
-# build_files.sh
-pip install -r requirements.txt
+#!/bin/bash
 
-# make migrations
-python manage.py migrate 
-python manage.py collectstatic
+pip install -r requirements.txt
+python manage.py migrate
+python manage.py collectstatic --noinput
+
